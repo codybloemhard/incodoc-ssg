540df59de6056711be73115a5b3c84659449c576
Cody Bloemhard <codybloemhard@gmail.com>
2026-02-23 00:17:23 +0100

    added: read entries from lines, write entries to lines, finished map;

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod tests;
 
+use std::fs::File;
+use std::io::prelude::*;
 use std::collections::HashMap;
 
 use simpleio::read_lines;
@@ -25,6 +27,10 @@ enum Commands {
     Map {
         src: String,
         dst: String,
+        #[clap(short = 'f', default_value_t = false, help = "Force: may overwrite.")]
+        force: bool,
+        #[clap(short = 'c', default_value_t = false, help = "Carry over version and enabled when overwriting.")]
+        carry: bool,
     },
     #[clap(short_flag = 'r', about = "Remove entry by either source or destination.")]
     Remove {
@@ -46,16 +52,24 @@ enum Commands {
     },
 }
 
-// TODO: read entries from lines, run map on call, write file
 fn main() {
     let args = Args::parse();
-    let lines = read_lines(args.path);
+    let lines = read_lines(&args.path);
+    let mut entries = Entries::from_lines(lines);
     match args.command {
         Commands::List => {
             println!("list");
         },
-        Commands::Map { src, dst } => {
+        Commands::Map { src, dst, force, carry } => {
             println!("map: src {src} to dst: {dst}");
+            match entries.map(src, dst, force, carry) {
+                MapResult::Noop => println!("  Nothing need to be done."),
+                MapResult::NewDst => println!("  New destination set."),
+                MapResult::NewSrc => println!("  New source set."),
+                MapResult::NewEntry => println!("  New source and destination set."),
+                MapResult::NewDstBlocked => println!("  Could not set new destination (use force)."),
+                MapResult::NewSrcBlocked => println!("  Could not set new source (use force)."),
+            }
         },
         Commands::Remove { src_or_dst } => {
             println!("remove: {src_or_dst}");
@@ -70,6 +84,14 @@ fn main() {
             println!("remove: {src_or_dst}, dry: {dry}");
         },
     }
+    let mut file = if let Ok(file) = File::create(&args.path) { file }
+    else {
+        eprintln!("Could not open file {} for writing", args.path);
+        return;
+    };
+    if file.write_all(&entries.to_lines().into_bytes()).is_err() {
+        eprintln!("Could not write to file {}!", args.path);
+    };
 }
 
 #[derive(Clone, Default, Hash, Debug, Eq, PartialEq, Ord, PartialOrd)]
@@ -98,6 +120,112 @@ enum MapResult {
 }
 
 impl Entries {
+    fn from_lines(lines: Vec<String>) -> Self {
+        let mut res = Self::default();
+        for (i, line) in lines.into_iter().enumerate() {
+            let mut split = line.split(',');
+            let src = if let Some(src) = split.next() {
+                let src = src.trim();
+                src.to_string()
+            } else {
+                eprintln!("Could not get src on line {i}!");
+                continue;
+            };
+            let dst = if let Some(dst) = split.next() {
+                let dst = dst.trim();
+                dst.to_string()
+            } else {
+                eprintln!("Could not get dst on line {i}!");
+                continue;
+            };
+            let version = if let Some(version_raw) = split.next() {
+                let mut split = version_raw.split('.');
+                let major = if let Some(major_raw) = split.next() {
+                    let major_raw = major_raw.trim();
+                    if let Ok(major) = major_raw.parse::<usize>() { major }
+                    else {
+                        eprintln!("Could not parse version major on line {i}!");
+                        continue;
+                    }
+                } else {
+                    eprintln!("Could not get version major on line {i}!");
+                    continue;
+                };
+                let minor = if let Some(minor_raw) = split.next() {
+                    if let Ok(minor) = minor_raw.parse::<usize>() { minor }
+                    else {
+                        eprintln!("Could not parse version minor on line {i}!");
+                        continue;
+                    }
+                } else {
+                    eprintln!("Could not get version minor on line {i}!");
+                    continue;
+                };
+                let fix = if let Some(fix_raw) = split.next() {
+                    if let Ok(fix) = fix_raw.parse::<usize>() { fix }
+                    else {
+                        eprintln!("Could not parse version fix on line {i}!");
+                        continue;
+                    }
+                } else {
+                    eprintln!("Could not get version fix on line {i}!");
+                    continue;
+                };
+                (major, minor, fix)
+            } else {
+                eprintln!("Could not get version on line {i}!");
+                continue;
+            };
+            let enabled = if let Some(enabled_raw) = split.next() {
+                let enabled_raw = enabled_raw.trim();
+                if enabled_raw == "enabled" {
+                    true
+                } else if enabled_raw == "disabled" {
+                    false
+                } else {
+                    eprintln!("Could not parse enabled on line {i}!");
+                    continue;
+                }
+            } else {
+                eprintln!("Could not get enabled on line {i}!");
+                continue;
+            };
+            res.entries.push(Entry {
+                src: src.clone(),
+                dst: dst.clone(),
+                version,
+                enabled,
+            });
+            let index = res.entries.len() - 1;
+            res.src_index.insert(src, index);
+            res.dst_index.insert(dst, index);
+        }
+        res
+    }
+
+    fn to_lines(&self) -> String {
+        let mut res = String::new();
+        for Entry { src, dst, version, enabled } in &self.entries {
+            res.push_str(src);
+            res.push_str(", ");
+            res.push_str(dst);
+            res.push_str(", ");
+            res.push_str(&version.0.to_string());
+            res.push('.');
+            res.push_str(&version.1.to_string());
+            res.push('.');
+            res.push_str(&version.2.to_string());
+            res.push_str(", ");
+            if *enabled {
+                res.push_str("enabled");
+            } else {
+                res.push_str("disabled");
+            }
+            res.push('\n');
+        }
+        res
+    }
+
     fn map(&mut self, src: String, dst: String, overwrite: bool, carry_over: bool)
         -> MapResult
     {
