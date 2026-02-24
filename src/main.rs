9f4ecf4f469afd255360d9ae51363eefe2c91f63
Cody Bloemhard <codybloemhard@gmail.com>
2026-02-24 19:36:41 +0100

    added: remove;

--- src/main.rs
+++ src/main.rs
@@ -62,16 +62,19 @@ fn main() {
         },
         Commands::Map { src, dst, force, carry } => {
             match entries.map(src, dst, force, carry) {
-                MapResult::Noop => println!("  Nothing need to be done."),
-                MapResult::NewDst => println!("  New destination set."),
-                MapResult::NewSrc => println!("  New source set."),
-                MapResult::NewEntry => println!("  New source and destination set."),
-                MapResult::NewDstBlocked => println!("  Could not set new destination (use force)."),
-                MapResult::NewSrcBlocked => println!("  Could not set new source (use force)."),
+                MapResult::Noop => println!("Nothing need to be done."),
+                MapResult::NewDst => println!("New destination set."),
+                MapResult::NewSrc => println!("New source set."),
+                MapResult::NewEntry => println!("New source and destination set."),
+                MapResult::NewDstBlocked => println!("Could not set new destination (use force)."),
+                MapResult::NewSrcBlocked => println!("Could not set new source (use force)."),
             }
         },
         Commands::Remove { src_or_dst } => {
-            println!("remove: {src_or_dst}");
+            if let Some(entry) = entries.remove(&src_or_dst) {
+                println!("Removed this entry: ");
+                println!("{}", entry.pretty_print());
+            }
         },
         Commands::Enable { src_or_dst } => {
             println!("remove: {src_or_dst}");
@@ -116,6 +119,32 @@ enum MapResult {
     NewEntry,
     NewDstBlocked,
     NewSrcBlocked,
+}
+
+impl Entry {
+    fn pretty_print(&self) -> String {
+        let mut res = String::new();
+        res.push_str("     source: ");
+        res.push_str(&self.src);
+        res.push('\n');
+        res.push_str("destination: ");
+        res.push_str(&self.dst);
+        res.push('\n');
+        res.push_str("    version: ");
+        res.push_str(&self.version.0.to_string());
+        res.push('.');
+        res.push_str(&self.version.1.to_string());
+        res.push('.');
+        res.push_str(&self.version.2.to_string());
+        res.push('\n');
+        res.push_str("    enabled: ");
+        if self.enabled {
+            res.push_str("yes");
+        } else {
+            res.push_str("no");
+        }
+        res
+    }
 }
 
 impl Entries {
@@ -204,26 +233,8 @@ impl Entries {
 
     fn list(&self) -> String {
         let mut res = String::new();
-        for Entry { src, dst, version, enabled } in &self.entries {
-            res.push_str("     source: ");
-            res.push_str(src);
-            res.push('\n');
-            res.push_str("destination: ");
-            res.push_str(dst);
-            res.push('\n');
-            res.push_str("    version: ");
-            res.push_str(&version.0.to_string());
-            res.push('.');
-            res.push_str(&version.1.to_string());
-            res.push('.');
-            res.push_str(&version.2.to_string());
-            res.push('\n');
-            res.push_str("    enabled: ");
-            if *enabled {
-                res.push_str("yes");
-            } else {
-                res.push_str("no");
-            }
+        for entry in &self.entries {
+            res.push_str(&entry.pretty_print());
             res.push_str("\n\n");
         }
         res.pop();
@@ -233,6 +244,10 @@ impl Entries {
     fn to_lines(&self) -> String {
         let mut res = String::new();
         for Entry { src, dst, version, enabled } in &self.entries {
+            // actual deleting of entries happens here
+            if src.is_empty() || dst.is_empty() {
+                continue;
+            }
             res.push_str(src);
             res.push_str(", ");
             res.push_str(dst);
@@ -299,6 +314,31 @@ impl Entries {
             self.src_index.insert(src, len);
             self.dst_index.insert(dst, len);
             MapResult::NewEntry
+        }
+    }
+
+    fn index_by_src_or_dst(&self, src_or_dst: &str) -> Option<usize> {
+        let src_res = self.src_index.get(src_or_dst);
+        if src_res.is_some() { return src_res.copied(); }
+        let dst_res = self.dst_index.get(src_or_dst);
+        if dst_res.is_some() { return dst_res.copied(); }
+        None
+    }
+
+    fn remove(&mut self, src_or_dst: &str) -> Option<Entry> {
+        if let Some(index) = self.index_by_src_or_dst(src_or_dst) {
+            self.src_index.remove(&self.entries[index].src);
+            self.dst_index.remove(&self.entries[index].dst);
+            let src = std::mem::take(&mut self.entries[index].src);
+            let dst = std::mem::take(&mut self.entries[index].dst);
+            Some(Entry {
+                src,
+                dst,
+                version: self.entries[index].version,
+                enabled: self.entries[index].enabled,
+            })
+        } else {
+            None
         }
     }
 }
